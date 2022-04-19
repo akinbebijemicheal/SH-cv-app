c4b1b8b1b57aaa03012dc67ce98603ab1c011f8a
akinbebijemicheal <michealakinbebije@gmail.com>
2022-04-18 22:25:26 -0700

    edit

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import BrowserRouter from 'react-router-dom/BrowserRouter'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -12,3 +13,9 @@ root.render(
 );
 
 
+
+ReactDOM.render((
+   <BrowserRouter basename={process.env.PUBLIC_URL}>
+     <App />
+   </BrowserRouter>
+), );
